7b498ce3e5fbb444a7e813a1d4749e77003715df
heltonandreazza <helton.prg@gmail.com>
2016-10-29 11:39:53 -0200

    create routes

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 'use strict';
+const express = require('express');
+const app = express();
+//my modules
+const chatCat = require('./app');
 
-console.log("welcome to chatcat");
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
+app.set('view engine', 'ejs'); //automatic import by express
+
+app.use('/', chatCat.router);
+
+app.listen(app.get('port'), () => {
+    console.log('chatcat is running at port 3000!');
+})
