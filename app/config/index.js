2ec9beb4d62c453f24dcb65d95162247aa30c02c
heltonandreazza <helton.prg@gmail.com>
2016-10-29 13:25:20 -0200

    config mlab

---
+++ app/config/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+if (process.env.NODE_ENV === 'production') {
+    //heroku enviroment variables
+    module.exports = {
+        host: process.env.host || "",
+        dbURI: process.env.dbURI
+    }
+} else {
+    module.exports = require('./development.json');
+}
