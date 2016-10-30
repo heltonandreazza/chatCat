144987dd0f7e4d3a009bbc9fa43101d072fb8354
heltonandreazza <helton.prg@gmail.com>
2016-10-30 09:17:40 -0200

    bridging socketio with express session

--- app/index.js
+++ app/index.js
@@ -8,6 +8,9 @@ let ioServer = app => {
     app.locals.chatrooms = [];
     const server = require('http').Server(app);
     const io = require('socket.io')(server);
+    io.use((socket, next) => {
+        require('./session')(socket.request, {}, next);
+    })
     require('./socket')(io, app);
     return server;
 }
